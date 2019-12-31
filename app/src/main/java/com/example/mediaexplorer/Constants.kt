faae60fec7a67fa16b7ef132549e4a9f508cb0bc
Michael Stepanchuk <v.stepan644872@gmail.com>
2019-12-31 16:45:04 +0200

    Add TABS_NAME

--- app/src/main/java/com/example/mediaexplorer/Constants.kt
+++ app/src/main/java/com/example/mediaexplorer/Constants.kt
@@ -1,12 +1,6 @@
 package com.example.mediaexplorer
 
-import android.Manifest
-
 object Constants {
     const val APPLICATION_TAG = "Media Explorer"
-    val REQUIRED_PERMISSIONS = arrayOf(
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.READ_EXTERNAL_STORAGE
-    )
+    val TABS_NAME = arrayListOf("Videos","Audios")
 }
