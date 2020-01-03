9e8489dd02e7547025d5a3d9fa8106ee333545b8
Michael Stepanchuk <v.stepan644872@gmail.com>
2020-01-03 20:30:32 +0200

    Improve RepositoryModule

--- app/src/main/java/com/example/mediaexplorer/di/RepositoryModule.kt
+++ app/src/main/java/com/example/mediaexplorer/di/RepositoryModule.kt
@@ -1,5 +1,6 @@
 package com.example.mediaexplorer.di
 
+import android.content.Context
 import com.example.mediaexplorer.repository.AudioRepository
 import com.example.mediaexplorer.repository.VideoRepository
 import org.koin.dsl.module
@@ -7,4 +8,7 @@ import org.koin.dsl.module
 val repositoryModule = module {
     single { VideoRepository(get()) }
     single { AudioRepository(get()) }
+    single { provideContentResolver(get()) }
 }
+
+private fun provideContentResolver(context: Context) = context.contentResolver
