25e6a3ee178b2e6d4d15c9fd10298a0c4103d2ff
Michael Stepanchuk <v.stepan644872@gmail.com>
2020-01-03 20:36:50 +0200

    Improve AudioRepository

--- app/src/main/java/com/example/mediaexplorer/repository/AudioRepository.kt
+++ app/src/main/java/com/example/mediaexplorer/repository/AudioRepository.kt
@@ -1,65 +1,36 @@
 package com.example.mediaexplorer.repository
 
-import android.content.Context
-import android.media.MediaMetadataRetriever
-import android.net.Uri
-import android.provider.MediaStore
+import android.content.ContentResolver
+import android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+import android.provider.MediaStore.Audio.Playlists.Members.*
 import com.example.mediaexplorer.entity.Audio
 import io.reactivex.Single
 import org.threeten.bp.Duration
-import java.io.File
 
-class AudioRepository(val context: Context) {
+class AudioRepository(private val contentResolver: ContentResolver) {
 
     fun getAllAudios(): Single<MutableList<Audio>> {
-        val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
-
-        val projection = arrayOf(
-            MediaStore.Audio.Media._ID,
-            MediaStore.Audio.Media.ARTIST,
-            MediaStore.Audio.Media.TITLE,
-            MediaStore.Audio.Media.DATA,
-            MediaStore.Audio.Media.SIZE
-        )
-
-        val cursor = context.contentResolver.query(
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-            projection,
-            selection,
-            null,
-            null
-        )!!
-
         val songs: MutableList<Audio> = ArrayList()
-        cursor.apply {
-            while (cursor.moveToNext()) {
+        val projection = arrayOf(_ID, ARTIST, TITLE, DATA, SIZE, DURATION)
+
+        contentResolver.query(EXTERNAL_CONTENT_URI, projection, null, null, null)?.apply {
+            while (moveToNext()) {
                 songs.add(
                     Audio(
-                        getInt(getColumnIndexOrThrow(MediaStore.Audio.Media._ID)),
-                        getString(getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)),
-                        getString(getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)),
-                        getString(getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)),
-                        getAudioDuration(
-                            context,
-                            getString(getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
-                        ),
-                        getLong(getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE))
+                        getInt(getColumnIndexOrThrow(_ID)),
+                        getString(getColumnIndexOrThrow(ARTIST)),
+                        getString(getColumnIndexOrThrow(TITLE)),
+                        getString(getColumnIndexOrThrow(DATA)),
+                        Duration.ofMillis(getLong(getColumnIndexOrThrow(DURATION))),
+                        getLong(getColumnIndexOrThrow(SIZE))
                     )
                 )
             }
+
+            close()
         }
-        cursor.close()
 
         return Single.just(songs)
-    }
-
-    private fun getAudioDuration(context: Context, videoPath: String): Duration {
-        val retriever = MediaMetadataRetriever()
-        retriever.setDataSource(context, Uri.fromFile(File(videoPath)))
-        val time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
-        retriever.release()
-
-        return Duration.ofMillis(time.toLong())
     }
 
 }
